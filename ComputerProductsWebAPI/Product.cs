566b701a817a3351161f033a9e7cbb91573ca47c
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-06-07 11:03:41 +0530

    Poco Class Added

---
+++ ComputerProductsWebAPI/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComputerProductsWebAPI
+{
+    public class Product
+    {
+        public int ProductCode { get; set; }
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+    }
+}
