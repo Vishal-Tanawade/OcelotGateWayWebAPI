1638e5f3290892751ffd7a2b29d7d57dff6af0f7
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-06-07 10:57:17 +0530

    mvc converted to api

--- GenuineCustomerWebAPI/Controllers/GenuineCustomerController.cs
+++ GenuineCustomerWebAPI/Controllers/GenuineCustomerController.cs
@@ -7,81 +7,117 @@ using System.Threading.Tasks;
 
 namespace GenuineCustomerWebAPI.Controllers
 {
-    public class GenuineCustomerController : Controller
-    {
-        // GET: GenuineCustomerController
-        public ActionResult Index()
+   
+        [Route("api/[controller]")]
+        [ApiController]
+        public class GenuineCustomerController : ControllerBase
         {
-            return View();
-        }
-
-        // GET: GenuineCustomerController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: GenuineCustomerController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: GenuineCustomerController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
+            private static List<Customer> CustomerList = new List<Customer>()
             {
-                return RedirectToAction(nameof(Index));
+              new Customer(){CutomerId=1,FirstName="Aaron",LastName="Hawkins",CellNumber="(660) 663-4518",Email="aron.hawkins@aol.com" },
+              new Customer(){CutomerId=2,FirstName="Hedy",LastName="Greene",CellNumber="(608) 265-2215",Email="hedy.greene@aol.com" },
+              new Customer(){CutomerId=3,FirstName="Melvin",LastName="Porter",CellNumber="(959) 119-8364",Email="melvin.porter@aol.com"}
+            };
+
+            // GET: api/<GenuineCustomerController>
+            [HttpGet]
+            public IEnumerable<Customer> Get()
+            {
+
+                return CustomerList.ToList();
             }
-            catch
+            // GET api/<ComputerProductsController>/5
+            [HttpGet("{id}")]
+            public IActionResult Get(int id)
             {
-                return View();
-            }
-        }
+                Customer customer = CustomerList.FirstOrDefault(c => c.CutomerId == id);
+                if (customer != null)
+                {
+                    return StatusCode(StatusCodes.Status200OK, customer);
+                }
+                else
+                {
+                    return BadRequest();
+                }
 
-        // GET: GenuineCustomerController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
 
-        // POST: GenuineCustomerController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
             }
-            catch
-            {
-                return View();
-            }
-        }
 
-        // GET: GenuineCustomerController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
 
-        // POST: GenuineCustomerController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
+            // POST api/<GenuineCustomerApiController>
+            [HttpPost]
+            public IActionResult Post([FromBody] Customer customer)
             {
-                return RedirectToAction(nameof(Index));
+
+                Customer cust = new Customer();
+                if (customer != null)
+                {
+                    cust = CustomerList.FirstOrDefault();
+                    if (cust != null)
+                    {
+                        int id = CustomerList.Max(c => c.CutomerId);
+                        customer.CutomerId = id + 1;
+                    }
+
+                    else
+                    {
+                        customer.CutomerId = 1;
+                    }
+
+
+                    CustomerList.Add(customer);
+                    return StatusCode(StatusCodes.Status201Created, customer);
+
+                }
+                else
+                {
+
+                    //return BadRequest();
+                    return StatusCode(StatusCodes.Status400BadRequest);
+                }
             }
-            catch
+
+            // PUT api/<GenuineCustomerApiController>/5
+
+            [HttpPut("{id}")]
+
+            public IActionResult Put(int id, [FromBody] Customer customer)
             {
-                return View();
+                Customer cust = CustomerList.FirstOrDefault(c => c.CutomerId == id);
+                if (cust != null)
+                {
+                    cust.FirstName = customer.FirstName;
+
+                    cust.LastName = customer.LastName;
+                    cust.CellNumber = customer.CellNumber;
+                    cust.Email = customer.Email;
+
+                    //return Ok();
+                    return StatusCode(StatusCodes.Status201Created, customer);
+                }
+                else
+                {
+                    return BadRequest();
+
+                }
+            }
+
+            // DELETE api/<GenuineCustomerApiController>/5
+
+            [HttpDelete("{id}")]
+            public IActionResult Delete(int id)
+            {
+                Customer cust = CustomerList.FirstOrDefault(c => c.CutomerId == id);
+                if (cust != null)
+                {
+                    CustomerList.Remove(cust);
+                    return Ok();
+                }
+                else
+                {
+                    return BadRequest();
+                }
             }
         }
     }
-}
+
