294b170d8f9525e51b6eb6f3568e9a4087495ae9
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-06-07 10:55:01 +0530

    MVC Controller with read/write action added

---
+++ GenuineCustomerWebAPI/Controllers/GenuineCustomerController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GenuineCustomerWebAPI.Controllers
+{
+    public class GenuineCustomerController : Controller
+    {
+        // GET: GenuineCustomerController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: GenuineCustomerController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: GenuineCustomerController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: GenuineCustomerController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: GenuineCustomerController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: GenuineCustomerController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: GenuineCustomerController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: GenuineCustomerController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
